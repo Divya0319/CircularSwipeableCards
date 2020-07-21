1257b9862190b86e48c5be51e2874ac60f1ae3c1
Divya Gupta <divygupta0319@gmail.com>
2020-07-21 13:25:08 +0530

    MainActivityViewModel updated, added asynchronous code callbacks

--- app/src/main/java/com/assignment/infinitelyswipablecards/MainActivityViewModel.kt
+++ app/src/main/java/com/assignment/infinitelyswipablecards/MainActivityViewModel.kt
@@ -1,10 +1,35 @@
 package com.assignment.infinitelyswipablecards
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.assignment.infinitelyswipablecards.network.ApiResponse
 import com.assignment.infinitelyswipablecards.network.Repository
+import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.disposables.CompositeDisposable
+import io.reactivex.schedulers.Schedulers
 
 /**
  * Created by Divya Gupta.
  */
 class MainActivityViewModel(val repository: Repository) : ViewModel() {
+    private val disposable = CompositeDisposable()
+    private val responseCardsAPI = MutableLiveData<ApiResponse>()
+    val cardsApiResponse: LiveData<ApiResponse> get() = responseCardsAPI
+
+    fun hitCardsApi() {
+        disposable.add(repository.executeCardsDataApiCall()
+            .subscribeOn(Schedulers.io())
+            .observeOn(AndroidSchedulers.mainThread())
+            .doOnSubscribe { responseCardsAPI.value = ApiResponse.loading() }
+            .subscribe(
+                { result -> responseCardsAPI.value = ApiResponse.success(result) },
+                { error -> responseCardsAPI.value = ApiResponse.error(error) }
+            ))
+    }
+
+
+    override fun onCleared() {
+        disposable.clear()
+    }
 }
