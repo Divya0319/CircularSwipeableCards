a32c9312c5439ba3e5b39471f687c9bcd0753193
Divya Gupta <divygupta0319@gmail.com>
2020-07-21 21:51:56 +0530

    UI changes done in Cards

--- app/src/main/java/com/assignment/infinitelyswipablecards/commons/Constants.kt
+++ app/src/main/java/com/assignment/infinitelyswipablecards/commons/Constants.kt
@@ -7,4 +7,6 @@ object Constants {
     const val BASE_URL = "https://gist.githubusercontent.com"
     const val CARDS_DATA_ENDPOINT =
         "/anishbajpai014/d482191cb4fff429333c5ec64b38c197/raw/b11f56c3177a9ddc6649288c80a004e7df41e3b9/HiringTask.json"
+
+    const val MAX_ELEVATION_FACTOR = 8
 }
