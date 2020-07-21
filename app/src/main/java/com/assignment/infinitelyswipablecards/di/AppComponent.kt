05adddd3b30d1dc113715fc724f7047b1398d684
Divya Gupta <divygupta0319@gmail.com>
2020-07-21 13:08:26 +0530

    Dagger App Component class created, which handles provision of services or dependencies to other clients or classes

---
+++ app/src/main/java/com/assignment/infinitelyswipablecards/di/AppComponent.kt
@@ -0,0 +1,23 @@
+package com.assignment.infinitelyswipablecards.di
+
+import com.assignment.infinitelyswipablecards.MainActivity
+import com.assignment.userinfoapp.di.UtilsModule
+import com.assignment.userinfoapp.network.Repository
+import dagger.Component
+import javax.inject.Singleton
+
+/**
+ * Created by Divya Gupta.
+ */
+
+/**
+ * Dagger App component class created to define interface methods taking relevant class as parameter where
+ * dependencies are to be injected
+ */
+@Component(modules = [AppModule::class, UtilsModule::class])
+@Singleton
+interface AppComponent {
+    fun doInjection(repository: Repository)
+    fun doInjection(mainActivity: MainActivity)
+
+}
