e066a6ac1b0a33d035278e3c2ebc63f889af42b7
Divya Gupta <divygupta0319@gmail.com>
2020-07-21 12:59:53 +0530

    Base url and endpoint added in Contants class to bed used in Retrofit

--- app/src/main/java/com/assignment/infinitelyswipablecards/commons/Constants.kt
+++ app/src/main/java/com/assignment/infinitelyswipablecards/commons/Constants.kt
@@ -4,6 +4,7 @@ package com.assignment.infinitelyswipablecards.commons
  * Created by Divya Gupta.
  */
 object Constants {
-    const val BASE_URL = ""
-    const val CARDS_DATA_ENDPOINT = ""
+    const val BASE_URL = "https://gist.githubusercontent.com"
+    const val CARDS_DATA_ENDPOINT =
+        "/anishbajpai014/d482191cb4fff429333c5ec64b38c197/raw/b11f56c3177a9ddc6649288c80a004e7df41e3b9/HiringTask.json"
 }
